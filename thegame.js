9aeee4ee57d6b8c97d84a4c8d87d14b33ea19159
Ulrics <66984492+Ulrics@users.noreply.github.com>
2025-05-17 16:25:34 -0700

    UI built & logic changed

--- thegame.js
+++ thegame.js
@@ -11,52 +11,135 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt("Do you want to play rock, paper, or scissors?");
-    return choice;
+const rock = document.querySelector('.battleButtonRock');
+const paper = document.querySelector('.battleButtonPaper');
+const scissors = document.querySelector('.battleButtonScissors');
+const dialog = document.querySelector('.dialog');
+const scoreComputer = document.querySelector('.scoreComputer');
+const scoreHuman = document.querySelector('.scoreHuman');
+
+function playRock() {
+  playRound("rock");
 }
 
+function playPaper() {
+  playRound("paper");
+}
 
+function playScissors() {
+  playRound("scissors");
+}
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
+rock.addEventListener("click", playRock);
+paper.addEventListener("click", playPaper);
+scissors.addEventListener("click", playScissors);
 
-    function playRound(humanChoice, computerChoice) {
-        computerChoice = getComputerChoice();
-        humanChoice = getHumanChoice().toLowerCase();
-        
-        if ((computerChoice == "paper" && humanChoice == "rock") || 
-            (computerChoice == "rock" && humanChoice == "scissors") || 
-            (computerChoice == "scissors" && humanChoice == "paper")) {
-            ++computerScore;
-            return console.log("You lose! The computer chose: " + computerChoice + " which beats your " + humanChoice);
-        }
-        
-        if (computerChoice == humanChoice){
-            console.log("It's a tie! nobody wins!"); 
-        }
+let humanScore = 0;
+let computerScore = 0;
 
-        else{
-            ++humanScore;
-            return console.log("You win! Your " + humanChoice + " beats the computer's " + computerChoice);
-        }
-
+function playRound(humanChoice) {
+    let computerChoice = getComputerChoice();
+    
+    if ((computerChoice == "paper" && humanChoice == "rock") || 
+        (computerChoice == "rock" && humanChoice == "scissors") || 
+        (computerChoice == "scissors" && humanChoice == "paper")) {
+        ++computerScore;
+        scoreComputer.textContent = computerScore;
+        dialog.style.color = "#FF3336";
+        dialog.textContent = "You lose! The computer chose: " + computerChoice + " which beats your " + humanChoice;
+        // return console.log("You lose! The computer chose: " + computerChoice + " which beats your " + humanChoice);
     }
-
-    for(let i = 0; i <= 4; i++){
-        playRound();
-    }
-    if (humanScore > computerScore){
-        return console.log("You win! Score: " + humanScore + ":" + computerScore);
-    }
-    if (humanScore == computerScore){
-        return console.log("It's a tie, nobody wins! Score: " + humanScore + ":" + computerScore);
+    
+    else if (computerChoice == humanChoice){
+        dialog.style.color = "black";
+        dialog.textContent = "It's a tie! nobody wins!";
+        // return console.log("It's a tie! nobody wins!"); 
     }
 
     else{
-        return console.log("You Lose! Score: " + humanScore + ":" + computerScore);
+        ++humanScore;
+        scoreHuman.textContent = humanScore;
+        dialog.style.color = "#09C762";
+        dialog.textContent = "You win! Your " + humanChoice + " beats the computer's " + computerChoice;
+        //return console.log("You win! Your " + humanChoice + " beats the computer's " + computerChoice);
     }
+    
+    if (humanScore === 5 || computerScore === 5){
+        return endGame();
+    }
+
 }
 
-console.log(playGame());
+function endGame(){
+
+    if (humanScore > computerScore){
+        dialog.style.color = "#09C762"
+        dialog.style.fontSize = "24px";
+        dialog.textContent = "You win! Score: " + humanScore + ":" + computerScore;
+    }
+    /*
+    else if (humanScore == computerScore){
+        dialog.style.color = "black";
+        dialog.style.fontSize = "24px";
+        dialog.textContent = "It's a tie, nobody wins! Score: " + humanScore + ":" + computerScore;
+    }
+    */
+    else{
+        dialog.style.color = "#FF3336";
+        dialog.style.fontSize = "24px";
+        dialog.textContent = "You Lose! Score: " + humanScore + ":" + computerScore;
+    }
+
+    const wrapper = document.querySelector(".wrapper"); 
+    const replayButton = document.createElement("div");
+    replayButton.textContent = "Replay";
+    replayButton.classList.add("replay");
+    wrapper.appendChild(replayButton);
+
+    const replayButtonCreated = document.querySelector('.replay');
+
+    replayButtonCreated.addEventListener("click", () => {
+    replayGame();
+    });
+
+    rock.style["background-color"] = "#F0F0F0";
+    paper.style["background-color"] = "#F0F0F0";
+    scissors.style["background-color"] = "#F0F0F0";
+
+    rock.removeEventListener("click", playRock);
+    paper.removeEventListener("click", playPaper);
+    scissors.removeEventListener("click", playScissors);
+
+    rock.style.cursor = "not-allowed";
+    paper.style.cursor = "not-allowed";
+    scissors.style.cursor = "not-allowed";
+}
+
+
+
+function replayGame(){
+    humanScore = 0;
+    computerScore = 0;
+    tie = 0;
+    dialog.style.color = "#717171";
+    dialog.style.fontSize = "16px";
+    dialog.style.fontWeight = "400";
+    dialog.textContent = "Make your move to start the game - First to five wins!";
+    scoreComputer.textContent = computerScore;
+    scoreHuman.textContent = humanScore;
+
+    rock.addEventListener("click", playRock);
+    paper.addEventListener("click", playPaper);
+    scissors.addEventListener("click", playScissors);
+   
+    rock.style.cursor = "pointer";
+    paper.style.cursor = "pointer";
+    scissors.style.cursor = "pointer";
+
+    rock.style["background-color"] = "";
+    paper.style["background-color"] = "";
+    scissors.style["background-color"] = "";
+
+    const replayButton = document.querySelector('.replay');
+    replayButton.remove();
+}
